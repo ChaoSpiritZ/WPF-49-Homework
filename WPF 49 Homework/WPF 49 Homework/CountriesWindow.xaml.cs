7568b3e3f2c88383693b4f79f646f0014cfe4f11
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-02-21 22:55:40 +0200

    Add files via upload

---
+++ WPF 49 Homework/WPF 49 Homework/CountriesWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_49_Homework
+{
+    /// <summary>
+    /// Interaction logic for CountriesWindow.xaml
+    /// </summary>
+    public partial class CountriesWindow : Window
+    {
+        public List<Country> countries = new List<Country>();
+
+        public CountriesWindow()
+        {
+            InitializeComponent();
+
+            
+            countries.Add(new Country("Germany", "Berlin", "Resources/GermanyFlag.jpg"));
+            countries.Add(new Country("Israel", "Jerusalem", "Resources/IsraelFlag.jpg"));
+            countries.Add(new Country("USA", "Washington", "Resources/USAFlag.jpg"));
+
+            CountryComboBox.ItemsSource = countries;
+            CountryComboBox.SelectedIndex = 0;
+        }
+    }
+
+    public class Country
+    {
+        public string CountryName { get; set; }
+        public string CapitalCity { get; set; }
+        public string ImagePath { get; set; }
+
+        public Country(string countryName, string capitalCity, string imagePath)
+        {
+            CountryName = countryName;
+            CapitalCity = capitalCity;
+            ImagePath = imagePath;
+        }
+    }
+}
---
+++ WPF 49 Homework/WPF 49 Homework/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_49_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int sum = 0;
+            int.TryParse(txt1.Text, out int num1);
+            int.TryParse(txt2.Text, out int num2);
+            sum = num1 + num2;
+            txtblock.Text = sum.ToString();
+        }
+
+        private void Next_Button_Click(object sender, RoutedEventArgs e)
+        {
+            CountriesWindow cWindow = new CountriesWindow();
+            cWindow.Show();
+        }
+    }
+}
